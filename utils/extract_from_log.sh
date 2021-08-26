5239b54b92d2d217e1a2cbb23897b5e3b5d550fa
Tobias Fladby <t_fladby@msn.com>
2021-08-26 12:27:52 +0200

    alterede script a bit

--- utils/extract_from_log.sh
+++ utils/extract_from_log.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 #extracts timestamp and rate from lines on the form:
-#<random chromium stuff> TOBIAS <timestamp> ms <rate> kbps
-awk /TOBIAS/ log.txt | awk {'print $3"\t"$5'} > log_tobias_extracted.txt
+#<random chromium stuff> TOBIAS <timestamp in ms> <rate in kbps>
+
+awk /TOBIAS/ log.txt | awk {'print $3"\t"$4'} > log_tobias_extracted.txt
