d5cd82b33213360ba4ff31b75b39a7ba14315b29
tobiasfl <33148663+tobiasfl@users.noreply.github.com>
2021-11-19 09:50:44 +0100

    Update limit_if_hbf.sh

--- utils/limit_if_hbf.sh
+++ utils/limit_if_hbf.sh
@@ -1,6 +1,7 @@
 IF=enp0s31f6
 DELAY=50ms
-BW=10mbit
+BW=3mbit
+QUEUELIMIT=12
 
 tc qdisc del dev $IF root
 
@@ -9,6 +10,6 @@ tc qdisc add dev $IF root handle 1: htb default 1
 tc class add dev $IF parent 1: classid 1:1 htb rate $BW ceil $BW
 
 #limit is maximum number of packets the qdisc may hold at a time
-tc qdisc add dev $IF parent 1:1 netem limit 1000 delay $DELAY loss 0.0%
+tc qdisc add dev $IF parent 1:1 netem limit $QUEUELIMIT delay $DELAY loss 0.0%
 
 tc filter add dev $IF protocol ip parent 1: prio 1 matchall flowid 1:1
