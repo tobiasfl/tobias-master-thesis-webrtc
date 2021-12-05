702587efcb740b0cd67eae2f543ed89c2a8c9051
Tobias <t_fladby@msn.com>
2021-12-05 15:58:30 +0100

    added so that schrome ignores certificate errors to start up scripts

--- testbed_setup/run_chrome_data_sender.sh
+++ testbed_setup/run_chrome_data_sender.sh
@@ -4,4 +4,4 @@ if [ $# -eq 0 ]; then
 fi
 #Assumes google chrome is installed on the machine
 
-google-chrome --use-fake-ui-for-media-stream --enable-logging=stderr --log_level=0 --v=1 "https://${1}:8888?data" > log.txt 2>&1
+google-chrome --use-fake-ui-for-media-stream --enable-logging=stderr --ignore-certificate-errors --log_level=0 --v=1 "https://${1}:8888?data" > log.txt 2>&1
--- testbed_setup/run_chrome_video_sender.sh
+++ testbed_setup/run_chrome_video_sender.sh
@@ -5,4 +5,4 @@ if [ $# -eq 0 ]; then
 fi
 #Assumes google chrome is installed on the machine
 
-google-chrome --use-fake-ui-for-media-stream --use-fake-device-for-media-stream --use-file-for-fake-video-capture=KristenAndSara_1280x720_60.y4m --enable-logging=stderr --log_level=0 --v=1 "https://${1}:8888?video" > log.txt 2>&1
+google-chrome --use-fake-ui-for-media-stream --use-fake-device-for-media-stream --use-file-for-fake-video-capture=KristenAndSara_1280x720_60.y4m --enable-logging=stderr --ignore-certificate-errors --log_level=0 --v=1 "https://${1}:8888?video" > log.txt 2>&1
