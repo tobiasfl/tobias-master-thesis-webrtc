361d78fe699c9a54b60856cc78375651089cb601
Tobias <t_fladby@msn.com>
2021-12-05 10:52:07 +0100

    started on some scripts

---
+++ testbed_setup/run_app.sh
@@ -0,0 +1,6 @@
+APP_LOCATION='${HOME}/webrtc-test-app'
+node ${APP_LOCATION}/dist/index.js
+
+#node ./dist/index.js
+
+
---
+++ testbed_setup/setup_app.sh
@@ -0,0 +1,6 @@
+cd $HOME
+git clone https://github.com/tobiasfl/webrtc-test-app.git
+cd webrtc-test-app
+npm install
+npm run build
+#Create certificates
