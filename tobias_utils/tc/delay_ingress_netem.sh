289cbc652f732df916d410343b39b9e75edf95da
Tobias <t_fladby@msn.com>
2021-12-15 13:29:39 +0100

    new script to limit rate only and fixed ingress script

--- tobias_utils/tc/delay_ingress_netem.sh
+++ tobias_utils/tc/delay_ingress_netem.sh
@@ -2,12 +2,8 @@ IF=enp0s31f6
 DELAY_MS=10
 
 modprobe ifb
-
-ip link set dev $IF up
-
+ip link set dev ifb0 up
 tc qdisc add dev $IF ingress
-
 tc filter add dev $IF parent ffff: \
-    protocol ip u32 match u32 0 0 flowid 1:1 action mirred egress redirect dev $IF
-
-tc qdisc add dev $IF root netem delay ${DELAY_MS}ms
+    protocol ip u32 match u32 0 0 flowid 1:1 action mirred egress redirect dev ifb0
+tc qdisc add dev ifb0 root netem delay ${DELAY_MS}ms
