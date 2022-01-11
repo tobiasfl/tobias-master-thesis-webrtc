8fa34957a03e925f0958d21a67518ffaf5344056
Tobias Fladby <t_fladby@msn.com>
2022-01-11 13:26:16 +0100

    changed receiver script

--- tobias_utils/setup_receiver.sh
+++ tobias_utils/setup_receiver.sh
@@ -8,4 +8,5 @@ ip addr list $IF
 #add default route through router
 ip route add default via 192.168.0.1
 
-
+#add static route to other subnet
+ip route add 10.0.0.0/24 via 192.168.0.1 dev enp0s31f6
