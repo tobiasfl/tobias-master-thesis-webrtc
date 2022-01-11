7e87c0946d8c317c7e23ad4e2289bf861906550d
Tobias <t_fladby@msn.com>
2022-01-11 11:16:59 +0100

    config of sender ip address

--- tobias_utils/setup_sender.sh
+++ tobias_utils/setup_sender.sh
@@ -1,2 +1,9 @@
+IF=enp0s31f6
+
+#configure interface
+ip link set dev $IF up
+ip addr add dev $IF 10.0.0.2/24
+ip addr list $IF
+
 #add default route through router
-ip route add default via 10.0.1.0
+ip route add default via 10.0.0.1
