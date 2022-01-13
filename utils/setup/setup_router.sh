a4061c7395a90952ef95bf1dde75f9f97412206d
Tobias <t_fladby@msn.com>
2022-01-13 15:06:57 +0100

    limit_if_hbf seems correct now!

--- utils/setup/setup_router.sh
+++ utils/setup/setup_router.sh
@@ -16,17 +16,9 @@ ip addr list $IFSND
 echo net.ipv4.ip_forward=1 > /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
 
-#limit bandwidth when forwarding
-DELAY_MS=10
-RATE_MBIT=3
-QUEUELIMIT=15
+#limit stuff
+bash ../tc/router.sh
 
-tc qdisc del dev $IFRECV root
-
-tc qdisc add dev $IFRECV root handle 1: htb default 1 tc class add dev $IFRECV parent 1: classid 1:1 htb rate ${RATE_MBIT}mbit ceil ${RATE_MBIT}mbit 
-
-#limit is maximum number of packets the qdisc may hold at a time
-tc qdisc add dev $IFRECV parent 1:1 handle 1001: netem limit ${QUEUELIMIT} delay ${DELAY_MS}ms loss 0.0%
-
-tc filter add dev $IFRECV protocol ip parent 1: prio 1 matchall flowid 1:1
-
+#turn off offloading
+bash turn_off_offloading.sh $IFSND
+bash turn_off_offloading.sh $IFRECV
--- utils/tc/limit_if_hbf.sh
+++ utils/tc/limit_if_hbf.sh
@@ -1,17 +1,32 @@
-IF=enp0s31f6
-DELAY_MS=10
+
+
+IFSND=enx00e096660248
+IFRCV=enp3s0
+IFVIR=ifb0
+DELAY_MS=50
 RATE_MBIT=3
 QUEUELIMIT=15
-#BDP_BYTES=$(echo "($DELAY_MS/1000.0)*($RATE_MBIT*1000000.0/8.0)" | bc -q -l)
-#BDP_PKTS=(echo "$BDP_BYTES/1250" | bc -q)
-#LIMIT_PKTS=$(echo "$BDP_PKTS+$QUEUELIMIT" | bc -q)
 
-tc qdisc del dev $IF root
+modprobe ifb
 
-tc qdisc add dev $IF root handle 1: htb default 1
-tc class add dev $IF parent 1: classid 1:1 htb rate ${RATE_MBIT}mbit ceil ${RATE_MBIT}mbit 
+ip link set dev ${IFVIR} up
 
-#limit is maximum number of packets the qdisc may hold at a time
-tc qdisc add dev $IF parent 1:1 handle 1001: netem limit ${QUEUELIMIT} delay ${DELAY_MS}ms loss 0.0%
+#send incoming packets on sender side to IFVIR
+tc qdisc del dev ${IFSND} ingress
+tc qdisc add dev ${IFSND} ingress
+tc filter add dev ${IFSND} parent ffff: \
+	protocol ip u32 match u32 0 0 flowid 1:1 action mirred egress redirect dev ${IFVIR}
 
-tc filter add dev $IF protocol ip parent 1: prio 1 matchall flowid 1:1
+#add delay to incoming packets
+tc qdisc del dev ${IFVIR} 
+tc qdisc add dev ${IFVIR} root handle 1: htb default 1
+tc class add dev ${IFVIR} parent 1: classid 1:1 htb rate 1000mbit ceil 1000mbit 
+tc qdisc add dev ${IFVIR} parent 1:1 handle 1001: netem limit 1000 delay ${DELAY_MS}ms loss 0.0%
+
+#limit bandwidth and queue size on outgoing packets
+tc qdisc del dev ${IFRCV} root
+tc qdisc add dev ${IFRCV} root handle 1: htb default 1
+tc class add dev ${IFRCV} parent 1: classid 1:1 htb rate ${RATE_MBIT}mbit ceil ${RATE_MBIT}mbit 
+tc qdisc add dev ${IFRCV} parent 1:1 handle 1001: pfifo limit ${QUEUELIMIT} 
+tc filter add dev ${IFRCV} protocol ip parent 1: prio 1 matchall flowid 1:1
+
