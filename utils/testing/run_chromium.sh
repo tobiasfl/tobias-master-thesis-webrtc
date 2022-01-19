d1ef4419adb0ec41f8c0d4e4d57e0e10300d34b4
Tobias <t_fladby@msn.com>
2022-01-19 19:11:32 +0100

    changed script for runnning chromium and mostly finished run_test

--- utils/testing/run_chromium.sh
+++ utils/testing/run_chromium.sh
@@ -1,4 +1,17 @@
-out/Default/chrome --use-fake-device-for-media-stream --use-file-for-fake-video-capture=KristenAndSara_1280x720_60.y4m --enable-logging=stderr --log_level=0 --v=3 --force-fieldtrials=WebRTC-Debugging-RtpDump/Enabled  https://localhost:8888 > log.txt 2>&1
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "USAGE: $0 <output log dir>"
+    exit 1
+fi
+
+
+if [ ! -d $1 ]; then
+    echo "Error: Directory $1 does not exist."
+    exit 1
+fi
+
+~/Code/chromium/src/out/Default/chrome --no-sandbox --use-fake-device-for-media-stream --use-file-for-fake-video-capture=KristenAndSara_1280x720_60.y4m --enable-logging=stderr --log_level=0 --v=3 --force-fieldtrials=WebRTC-Debugging-RtpDump/Enabled  https://localhost:8888 > $1/log.txt 2>&1
 
 #This enables SRTP packet dumping
 #--enable-logging --v=3 --force-fieldtrials=WebRTC-Debugging-RtpDump/Enabled
--- utils/testing/run_test.sh
+++ utils/testing/run_test.sh
@@ -6,17 +6,29 @@ if [ $# -lt 5 ]; then
     exit 1
 fi
 
+out_dir=$5/$1mbit_$2ms_$3pkts_$4sec_chromium_test
 #Create directory for output data
 if [ -d $5 ]; then
-    mkdir $5/$1mbit_$2ms_$3pkts_$4sec_chromium_test
+    mkdir $out_dir
 else
     echo "Error: Directory $5 does not exist."
     exit 1
 fi
 
 #ssh into router, run limitation script with arguments from this script
-ssh -t tobias@10.0.0.1 "sudo ~/Code/tobias-master-thesis-webrtc/utils/tc/limit_router.sh $1 $2 $3"
+ssh -t tobias@10.0.0.1 "sudo bash ~/Code/tobias-master-thesis-webrtc/utils/tc/limit_router.sh $1 $2 $3"
 
-#start tcpdump: tcpdump -i enp0s31f6 -w ./sctp.pcap
+#TODO: ssh into receiver and start chrome from terminal(and maybe exit after everything is finished)
+
+#start tcpdump, store pid so it can be exited after chromium has finished its run
+#TODO: may also start tcpdumps at router and receiver if neccessary
+tcpdump -i enp0s31f6 -w $out_dir/if_dump.pcap &
+tcpdump_pid=$!
 
 #run chromium for some time before exiting
+timeout $4 bash run_chromium.sh $out_dir
+chromium_pid=$!
+
+wait $chromium_pid
+#stop dumping once, chromium run finished
+kill $tcpdump_pid
