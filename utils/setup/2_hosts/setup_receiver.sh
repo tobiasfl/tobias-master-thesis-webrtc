dcc599b3f2fbef26aa144a9eb41fdd09cc429895
Tobias <t_fladby@msn.com>
2022-02-05 12:01:39 +0100

    made 2hosts scripts set static ip routes

--- utils/setup/2_hosts/setup_receiver.sh
+++ utils/setup/2_hosts/setup_receiver.sh
@@ -5,6 +5,9 @@ ip link set dev $IF up
 ip addr add dev $IF 192.168.0.2/24
 ip addr list $IF
 
+#add static route to other subnet
+ip route add 10.0.0.0/24 via 192.168.0.1 dev enp0s31f6
+
 #turn off offloading
 bash ~/Code/tobias-master-thesis-webrtc/utils/setup/turn_off_offloading.sh $IF
 
--- utils/setup/setup_receiver.sh
+++ utils/setup/setup_receiver.sh
@@ -8,8 +8,6 @@ ip addr list $IF
 #add static route to other subnet
 ip route add 10.0.0.0/24 via 192.168.0.1 dev enp0s31f6
 
-
-
 #turn off offloading
 bash turn_off_offloading.sh $IF
 
