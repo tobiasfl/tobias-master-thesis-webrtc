70a9fe37dfe7628c774c47d630ead4b2ab2df070
Tobias <t_fladby@msn.com>
2022-02-05 12:30:23 +0100

    added script to copy chromium code to repo

---
+++ chromium_code/copy_to_repo.sh
@@ -0,0 +1,3 @@
+cp /home/tobias/Code/chromium/src/third_party/webrtc/modules/congestion_controller/goog_cc /home/tobias/Code/all_changed_files -r
+cp /home/tobias/Code/chromium/src/third_party/webrtc/media/sctp /home/tobias/Code/all_changed_files -r
+cp /home/tobias/Code/chromium/src/third_party/usrsctp/usrsctplib /home/tobias/Code/all_changed_files -r
--- utils/setup/2_hosts/setup_sender.sh
+++ utils/setup/2_hosts/setup_sender.sh
@@ -5,8 +5,5 @@ ip link set dev $IF up
 ip addr add dev $IF 10.0.0.2/24
 ip addr list $IF
 
-#add static route to other subnet
-ip route add 192.168.0.0/24 via 10.0.0.1 dev enp0s31f6
-
 #turn off offloading
 bash /home/tobias/Code/tobias-master-thesis-webrtc/utils/setup/turn_off_offloading.sh $IF
