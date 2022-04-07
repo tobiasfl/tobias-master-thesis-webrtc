948b2782c8599ea7440bc34debbf3fe86a689b34
Tobias <t_fladby@msn.com>
2022-04-07 09:01:36 +0200

    changed offloading script

--- utils/setup/turn_off_offloading.sh
+++ utils/setup/turn_off_offloading.sh
@@ -5,7 +5,9 @@ fi
 
 ethtool -K $1 gro off
 ethtool -K $1 tso off
-ethtool -K $1 lro off
-ethtool -K $1 ufo off
-ethtool -K $1 gso off
-
+ethtool -K $1 sg off
+#ethtool -K $1 lro off
+ethtool -A $1 autoneg off tx off rx off
+#ethtool -K $1 ufo off
+#ethtool -K $1 gso off
+ethtool -K $1 tx-gso-partial off
