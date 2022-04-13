4ffa0626b13120c089b79f1bcb27124fcdf92974
Tobias <t_fladby@msn.com>
2022-04-13 17:54:36 +0200

    trying j script

--- utils/testing/2_hosts/run_sanity_test.sh
+++ utils/testing/2_hosts/run_sanity_test.sh
@@ -2,9 +2,9 @@
 RECEIVER=192.168.0.2
 SENDER=10.0.0.2
 
-SS_LEN=115
-TEST_LEN=120
-TCP_START_TIME=60
+SS_LEN=49
+TEST_LEN=50
+TCP_START_TIME=0
 
 out_dir=sanity_check$(date --rfc-3339=date)
 
@@ -14,15 +14,16 @@ enabled_features="none"
 
 CHROMIUM_OVERRIDE="chromium"
 
+RTT=100
+OWD=$(($RTT / 2))
 
 mkdir $out_dir
 
 sudo echo "applying limits at receiver"
-ssh -t tobias@$RECEIVER "sudo bash ~/Code/tobias-master-thesis-webrtc/utils/testing/2_hosts/teardown_receiver.sh"
-ssh -t tobias@$RECEIVER "sudo bash ~/Code/tobias-master-thesis-webrtc/utils/testing/2_hosts/limit_receiver.sh 100"
+ssh -t tobias@$RECEIVER "sudo bash ~/Code/tobias-master-thesis-webrtc/utils/testing/2_hosts/setup_ingress_rate_limit.sh enp0s31f6 2 $OWD 17"
 
 echo "applying limits at sender"
-sudo bash ~/Code/tobias-master-thesis-webrtc/utils/testing/2_hosts/limit_sender.sh 2 17
+sudo bash ~/Code/tobias-master-thesis-webrtc/utils/testing/2_hosts/setup_ingress_rate_limit.sh enp0s31f6 2 $OWD 17
 
 read -p "start iperf server at receiver"
 
@@ -32,7 +33,8 @@ timeout $SS_LEN bash ~/Code/tobias-master-thesis-webrtc/utils/ss/ss_output.sh $R
 
 (sleep $TCP_START_TIME && iperf -c $RECEIVER -Z reno -n 1000M)&
 
-timeout $TEST_LEN bash ~/Code/tobias-master-thesis-webrtc/utils/testing/run_chromium.sh $out_dir $url $enabled_features $CHROMIUM_OVERRIDE
+#timeout $TEST_LEN bash ~/Code/tobias-master-thesis-webrtc/utils/testing/run_chromium.sh $out_dir $url $enabled_features $CHROMIUM_OVERRIDE
+sleep $TEST_LEN
 
 sudo killall tcpdump iperf
 
